92a8496a1b90732319e60ee779a7129408b5cf33
Lisa Song <christina.songlijing@gmail.com>
2017-07-30 12:07:10 -0700

    Two Sum

---
+++ 1 Two Sum.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                    
+class Solution(object): #trading space for speed
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hashTable = {}
+        n = len(nums)
+        for i, num in enumerate(nums):
+            try:
+                return [hashTable[num], i]
+            except KeyError:
+                hashTable[target-num] = i            
