7807620b1329a82f44a885611b271cf1eaf154ed
Lisa Song <christina.songlijing@gmail.com>
2017-08-03 22:41:50 -0700

    Reverse String

---
+++ 344 Reverse String.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ''.join(reversed(s))
+        
+class Solution(object):
+    def reverseString(self, s):
+        return s[::-1]
